eb153f15ef298639013b0a30cff49276b253d327
Akila Liyanage <akila.liyanage@madmobile.com>
2021-04-25 14:58:20 +0530

    cors installed and configured

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 require('dotenv/config')
 
 //Import Routes
 const navBarRoutes = require('./routes/NavBarItemRoutes')
 
 //Middleware
+app.use(bodyParser.json())
+app.use(cors())
 app.use('/nav-items',navBarRoutes);
 
 
